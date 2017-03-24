dd973ec1e7aa3a4a1b6036075d87c88150ac0b81
Amanda Bater <amandabater@amandas-mbp.lan>
2017-03-24 17:10:34 +0000

    creates course model

--- courses/models.py
+++ courses/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=225)
+    description = models.TextField()
